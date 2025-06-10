c2b253faa670b17119fe0353e8b19e8a0e33864e
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-06-10 09:34:09 +0700

    Add project files.

---
+++ KodeProduk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300137
+{
+    public class KodeProduk
+    {
+        string kodeProduk;
+
+        public KodeProduk(string kode)
+        {
+            if (kode.Length != 4)
+            {
+                throw new ArgumentException("Kode produk harus terdiri dari 10 karakter.");
+            }
+            this.kodeProduk = kode;
+        }
+
+        public string Kode
+        {
+            get { return kodeProduk; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace modul4_103022300137;
+{
+    public class Program
+    {
+        enum State { START, GAME, PAUSE, EXIT };
+        public static void Main()
+        {
+            State state = State.START;
+            string[] screenName = { "START", "GAME", "PAUSE", "EXIT" };
+            while (state != State.EXIT)
+        {
+            Console.WriteLine(screenName[(int)state] + " SCREEN");
+            Console.Write("Enter Command : ");
+
+            string command = Console.ReadLine();
+            switch (state)
+            {
+                case State.START:
+                    if (command == "ENTER")
+                        state = State.GAME;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.START;
+                    break;
+                case State.GAME:
+                    if (command == "ESC")
+                        state = State.PAUSE;
+                    else
+                        state = State.GAME;
+                    break;
+                case State.PAUSE:
+                    if (command == "BACK")
+                        state = State.GAME;
+                    else if (command == "HOME")
+                        state = State.START;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.PAUSE;
+                    break;
+            }
+        }
+        Console.WriteLine("EXIT SCREEN");
+    }
+}
